77e934e1b2fca09fc9f1a5c4dbbde23613040e99
Leopold-coder <leopoldsaiko@icloud.com>
2025-06-29 16:15:01 +0200

    Add win detection: stop game when 3 in a row

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -28,4 +28,27 @@ public class Board {
         grid[row][col] = symbol;
         return true;
     }
+    public boolean hasWon(char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (grid[i][0] == symbol && grid[i][1] == symbol && grid[i][2] == symbol) {
+                return true;
+            }
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (grid[0][j] == symbol && grid[1][j] == symbol && grid[2][j] == symbol) {
+                return true;
+            }
+        }
+
+        if (grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol) {
+            return true;
+        }
+
+        if (grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
+    }
 }
